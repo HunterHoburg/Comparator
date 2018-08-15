7ec5d8d1223ce0594d6eacf8f1eaa4ef16a8e553
Hunter Hoburg <hhoburg@gmail.com>
2018-08-15 10:30:05 -0700

    adds compare object function and tests

--- src/__tests__/objectComparer/index.js
+++ src/__tests__/objectComparer/index.js
@@ -19,25 +19,54 @@ describe('objectComparer', () => {
         expect(objectComparer.compareKeys({}, {notTest: 2})).toBe(false);
     });
 
-    it('should compare complex objects', () => {
-        const obj1 = {
-            key1: 'something',
-            key2: {
-                key2a: 2
-            }
-        };
-        const obj2 = {
-            key1: 'something else',
-            key2: {
-                key2a: 3
-            }
-        };
-        const obj3 = {
-            key1: 'something more else',
-            key2: 4
-        };
+    const obj1 = {
+        key1: 'something',
+    }
+    const obj2 = {
+        key1: 'something else'
+    }
+    const obj3 = {
+        key1: 4
+    }
+    const obj4 = {
+        key1: null,
+        key2: {
+            key2a: 'nested key'
+        }
+    }
+    const obj5 = {
+        key1: undefined,
+        key2: {
+            key2a: []
+        }
+    }
+    const obj6 = {
+        key1: 'something',
+        key2: 'something else'
+    }
 
+    it('should compare complex object keys', () => {
+        expect(objectComparer.compareKeys(obj1, obj2)).toBe(true);
+        expect(objectComparer.compareKeys(obj1, obj3)).toBe(true);
+        expect(objectComparer.compareKeys(obj1, obj4)).toBe(false);
+        expect(objectComparer.compareKeys(obj5, obj4)).toBe(true);
+    });
+
+    it('should compare complex object values', () => {
+        expect(objectComparer.compareValueTypes(obj1, obj2)).toBe(true);
+        expect(objectComparer.compareValueTypes(obj1, obj3)).toBe(false);
+        expect(objectComparer.compareValueTypes(obj2, obj4)).toBe(false);
+        expect(objectComparer.compareValueTypes(obj1, obj6)).toBe(false);
+        expect(objectComparer.compareValueTypes(obj4, obj5)).toBe(false);
+        expect(objectComparer.compareValueTypes(obj4, obj5)).toBe(false);
+    });
+
+    it('should compare everything about an object', () => {
         expect(objectComparer.compareObjects(obj1, obj2)).toBe(true);
         expect(objectComparer.compareObjects(obj1, obj3)).toBe(false);
+        expect(objectComparer.compareObjects(obj2, obj4)).toBe(false);
+        expect(objectComparer.compareObjects(obj1, obj6)).toBe(false);
+        expect(objectComparer.compareObjects(obj4, obj5)).toBe(false);
+        expect(objectComparer.compareObjects(obj4, obj5)).toBe(false);
     })
 });
--- src/objectComparer/index.js
+++ src/objectComparer/index.js
@@ -4,17 +4,34 @@ const typeComparer = new TypeComparer();
 
 export class ObjectComparer {
     compareKeys(obj1, obj2) {
+        if (!typeComparer.compareTypes(obj1, obj2)) return false;
         for (let key in obj1) {
             if (!obj2.hasOwnProperty(key)) return false;
+            if (typeof obj1[key] === 'object' && !Array.isArray(obj1[key])) return this.compareKeys(obj1[key], obj2[key]);
         }
         for (let key in obj2) {
             if (!obj1.hasOwnProperty(key)) return false;
+            if (typeof obj2[key] === 'object' && !Array.isArray(obj2[key])) return this.compareKeys(obj2[key], obj1[key]);
+        }
+        return true;
+    }
+
+    compareValueTypes(obj1, obj2) {
+        if (!typeComparer.compareTypes(obj1, obj2)) return false;
+        for (let key in obj1) {
+            if (!typeComparer.compareTypes(obj1[key], obj2[key])) return false;
+            if (typeof obj1[key] === 'object' && !Array.isArray(obj1[key])) {
+                return this.compareValueTypes(obj1[key], obj2[key]);
+            }
+        }
+        for (let key in obj2) {
+            if (!typeComparer.compareTypes(obj2[key], obj1[key])) return false;
+            if (typeof obj2[key] === 'object' && !Array.isArray(obj2[key])) return this.compareValueTypes(obj2[key], obj1[key]);
         }
         return true;
     }
 
     compareObjects(obj1, obj2) {
-
-        return true;
+        return (this.compareKeys(obj1, obj2) && this.compareValueTypes(obj1, obj2));
     }
 }
